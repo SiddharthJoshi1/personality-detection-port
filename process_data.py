5190f3ae43f91494c4a7fc81ccbcabd0f1c1d7f2
Sid <siddharthjoshi1@hotmail.com>
2025-01-12 22:43:17 +0000

    port over conv net classes to keras using an llm

--- process_data.py
+++ process_data.py
@@ -1,11 +1,9 @@
 import numpy as np
-import theano
-import cPickle
+import pickle
 from collections import defaultdict
 import sys, re
 import pandas as pd
 import csv
-import getpass
 
 
 def build_data_cv(datafile, cv=10, clean_string=True):
@@ -15,34 +13,34 @@ def build_data_cv(datafile, cv=10, clean_string=True):
     revs = []
     vocab = defaultdict(float)
 
-    with open(datafile, "rb") as csvf:
-        csvreader=csv.reader(csvf,delimiter=',',quotechar='"')
-        first_line=True
+    with open(datafile, "r", encoding="utf-8") as csvf:
+        csvreader = csv.reader(csvf, delimiter=',', quotechar='"')
+        first_line = True
         for line in csvreader:
             if first_line:
-                first_line=False
+                first_line = False
                 continue
-            status=[]
-            sentences=re.split(r'[.?]', line[1].strip())
+            status = []
+            sentences = re.split(r'[.?]', line[1].strip())
             try:
                 sentences.remove('')
             except ValueError:
-                None
+                pass
 
             for sent in sentences:
                 if clean_string:
                     orig_rev = clean_str(sent.strip())
-                    if orig_rev=='':
-                            continue
+                    if orig_rev == '':
+                        continue
                     words = set(orig_rev.split())
                     splitted = orig_rev.split()
-                    if len(splitted)>150:
-                        orig_rev=[]
-                        splits=int(np.floor(len(splitted)/20))
+                    if len(splitted) > 150:
+                        orig_rev = []
+                        splits = int(np.floor(len(splitted) / 20))
                         for index in range(splits):
-                            orig_rev.append(' '.join(splitted[index*20:(index+1)*20]))
-                        if len(splitted)>splits*20:
-                            orig_rev.append(' '.join(splitted[splits*20:]))
+                            orig_rev.append(' '.join(splitted[index * 20:(index + 1) * 20]))
+                        if len(splitted) > splits * 20:
+                            orig_rev.append(' '.join(splitted[splits * 20:]))
                         status.extend(orig_rev)
                     else:
                         status.append(orig_rev)
@@ -54,20 +52,19 @@ def build_data_cv(datafile, cv=10, clean_string=True):
                 for word in words:
                     vocab[word] += 1
 
-
-            datum  = {"y0":1 if line[2].lower()=='y' else 0,
-                  "y1":1 if line[3].lower()=='y' else 0,
-                  "y2":1 if line[4].lower()=='y' else 0,
-                  "y3":1 if line[5].lower()=='y' else 0,
-                  "y4":1 if line[6].lower()=='y' else 0,
-                  "text": status,
-                  "user": line[0],
-                  "num_words": np.max([len(sent.split()) for sent in status]),
-                  "split": np.random.randint(0,cv)}
+            datum = {"y0": 1 if line[2].lower() == 'y' else 0,
+                     "y1": 1 if line[3].lower() == 'y' else 0,
+                     "y2": 1 if line[4].lower() == 'y' else 0,
+                     "y3": 1 if line[5].lower() == 'y' else 0,
+                     "y4": 1 if line[6].lower() == 'y' else 0,
+                     "text": status,
+                     "user": line[0],
+                     "num_words": np.max([len(sent.split()) for sent in status]),
+                     "split": np.random.randint(0, cv)}
             revs.append(datum)
 
-
     return revs, vocab
+
 
 def get_W(word_vecs, k=300):
     """
@@ -75,14 +72,15 @@ def get_W(word_vecs, k=300):
     """
     vocab_size = len(word_vecs)
     word_idx_map = dict()
-    W = np.zeros(shape=(vocab_size+1, k), dtype=theano.config.floatX)
-    W[0] = np.zeros(k, dtype=theano.config.floatX)
+    W = np.zeros(shape=(vocab_size + 1, k), dtype=np.float32)
+    W[0] = np.zeros(k, dtype=np.float32)
     i = 1
     for word in word_vecs:
         W[i] = word_vecs[word]
         word_idx_map[word] = i
         i += 1
     return W, word_idx_map
+
 
 def load_bin_vec(fname, vocab):
     """
@@ -92,21 +90,22 @@ def load_bin_vec(fname, vocab):
     with open(fname, "rb") as f:
         header = f.readline()
         vocab_size, layer1_size = map(int, header.split())
-        binary_len = np.dtype(theano.config.floatX).itemsize * layer1_size
-        for line in xrange(vocab_size):
+        binary_len = np.dtype(np.float32).itemsize * layer1_size
+        for _ in range(vocab_size):
             word = []
             while True:
-                ch = f.read(1)
+                ch = f.read(1).decode('latin1')
                 if ch == ' ':
                     word = ''.join(word)
                     break
                 if ch != '\n':
                     word.append(ch)
             if word in vocab:
-               word_vecs[word] = np.fromstring(f.read(binary_len), dtype=theano.config.floatX)
+                word_vecs[word] = np.frombuffer(f.read(binary_len), dtype=np.float32)
             else:
                 f.read(binary_len)
     return word_vecs
+
 
 def add_unknown_words(word_vecs, vocab, min_df=1, k=300):
     """
@@ -115,8 +114,9 @@ def add_unknown_words(word_vecs, vocab, min_df=1, k=300):
     """
     for word in vocab:
         if word not in word_vecs and vocab[word] >= min_df:
-            word_vecs[word] = np.random.uniform(-0.25,0.25,k)
-            print word
+            word_vecs[word] = np.random.uniform(-0.25, 0.25, k)
+            print(word)
+
 
 def clean_str(string, TREC=False):
     """
@@ -128,16 +128,16 @@ def clean_str(string, TREC=False):
     string = re.sub(r"\'ve", " have ", string)
     string = re.sub(r"n\'t", " not ", string)
     string = re.sub(r"\'re", " are ", string)
-    string = re.sub(r"\'d" , " would ", string)
+    string = re.sub(r"\'d", " would ", string)
     string = re.sub(r"\'ll", " will ", string)
     string = re.sub(r",", " , ", string)
     string = re.sub(r"!", " ! ", string)
     string = re.sub(r"\(", " ( ", string)
     string = re.sub(r"\)", " ) ", string)
     string = re.sub(r"\?", " \? ", string)
-#    string = re.sub(r"[a-zA-Z]{4,}", "", string)
     string = re.sub(r"\s{2,}", " ", string)
     return string.strip() if TREC else string.strip().lower()
+
 
 def clean_str_sst(string):
     """
@@ -147,36 +147,37 @@ def clean_str_sst(string):
     string = re.sub(r"\s{2,}", " ", string)
     return string.strip().lower()
 
+
 def get_mairesse_features(file_name):
-    feats={}
-    with open(file_name, "rb") as csvf:
-        csvreader=csv.reader(csvf,delimiter=',',quotechar='"')
+    feats = {}
+    with open(file_name, "r", encoding="utf-8") as csvf:
+        csvreader = csv.reader(csvf, delimiter=',', quotechar='"')
         for line in csvreader:
-            feats[line[0]]=[float(f) for f in line[1:]]
+            feats[line[0]] = [float(f) for f in line[1:]]
     return feats
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     w2v_file = sys.argv[1]
     data_folder = sys.argv[2]
     mairesse_file = sys.argv[3]
-    print "loading data...",
+    print("loading data...", end=" ")
     revs, vocab = build_data_cv(data_folder, cv=10, clean_string=True)
-    num_words=pd.DataFrame(revs)["num_words"]
+    num_words = pd.DataFrame(revs)["num_words"]
     max_l = np.max(num_words)
-    print "data loaded!"
-    print "number of status: " + str(len(revs))
-    print "vocab size: " + str(len(vocab))
-    print "max sentence length: " + str(max_l)
-    print "loading word2vec vectors...",
+    print("data loaded!")
+    print("number of status:", len(revs))
+    print("vocab size:", len(vocab))
+    print("max sentence length:", max_l)
+    print("loading word2vec vectors...", end=" ")
     w2v = load_bin_vec(w2v_file, vocab)
-    print "word2vec loaded!"
-    print "num words already in word2vec: " + str(len(w2v))
+    print("word2vec loaded!")
+    print("num words already in word2vec:", len(w2v))
     add_unknown_words(w2v, vocab)
     W, word_idx_map = get_W(w2v)
     rand_vecs = {}
     add_unknown_words(rand_vecs, vocab)
     W2, _ = get_W(rand_vecs)
     mairesse = get_mairesse_features(mairesse_file)
-    cPickle.dump([revs, W, W2, word_idx_map, vocab, mairesse], open("essays_mairesse.p", "wb"))
-    print "dataset created!"
-
+    pickle.dump([revs, W, W2, word_idx_map, vocab, mairesse], open("essays_mairesse.p", "wb"))
+    print("dataset created!")
